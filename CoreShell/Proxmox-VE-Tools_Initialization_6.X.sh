df7fda16d84b5b387419635138ffe9fa333c3fe4
MeowLove <32319912+MeowLove@users.noreply.github.com>
2019-07-28 11:55:11 +0800

    Version 1.0.2
    [Version 1.0.2]
    
    Update:
    1. PVE 6.X Initialization Tools

--- CoreShell/Proxmox-VE-Tools_Initialization_6.X.sh
+++ CoreShell/Proxmox-VE-Tools_Initialization_6.X.sh
@@ -33,12 +33,16 @@ echo -e "\033[37m Modify Subscription (pve-no-subscription)... \033[0m"
 mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
 echo -e 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription
 deb http://deb.debian.org/debian experimental main' > /etc/apt/sources.list.d/pve-enterprise.list
+wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 echo -e "\033[32m Modify Subscription (pve-no-subscription)...OK \033[0m"
 sleep 1s
 
 echo -e "\033[37m Update Proxmox and Debian to the latest and install common software... \033[0m"
+apt remove os-prober -y
 apt update -y && apt dist-upgrade -y
-apt install -y vim curl wget lrzsz odhcp6c nano isc-dhcp-server net-tools sudo
+apt update -y && apt full-upgrade -y
+apt install -y vim curl wget lrzsz odhcp6c nano isc-dhcp-server net-tools sudo p7zip-full
 echo -e "\033[32m Update Proxmox and Debian to the latest and install common software...OK \033[0m"
 sleep 1s
 
@@ -87,6 +91,12 @@ range 10.0.0.10 10.0.0.254;
 echo -e "\033[32m Configure DHCP (10.0.0.0/8, routers 10.0.0.1)...OK \033[0m"
 sleep 1s
 
+echo -e "\033[37m Update Gurb2 to clean up invalid Debian kernel... \033[0m"
+dpkg --list | egrep -i --color 'linux-image|linux-headers'
+apt remove linux-image-amd64 'linux-image-4.19*' -y
+update-grub
+echo -e "\033[32m Update Gurb2 to clean up invalid Debian kernel...OK \033[0m"
+sleep 1s
 
 echo -e "\n"
 echo -e "\033[32m Please follow the tutorial to configure the Networks NIC settings (vim /etc/network/interfaces) \033[0m"
